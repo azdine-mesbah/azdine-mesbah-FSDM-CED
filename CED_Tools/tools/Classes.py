3eef34cd26e1666c1b766d7ebe93b5778e97ed69
A.mesbah <azdine.mesbah@gmail.com>
2021-12-23 15:20:39 +0100

    change TimeStapedModel inheritence

--- CED_Tools/tools/Classes.py
+++ CED_Tools/tools/Classes.py
@@ -1,13 +1,13 @@
 from django.db import models
 from softdelete.models import SoftDeleteObject
 
-class TimeStampedModel(SoftDeleteObject, models.Model):
+class TimeStampedModel(SoftDeleteObject):
     """
     An abstract base class model that provides self-updating
     ``created_at`` and ``updated_at`` fields.
     """
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
     class Meta:
         abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
