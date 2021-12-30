21b434d6d88142ba0e054e2b87c42db480894ef2
A.mesbah <azdine.mesbah@gmail.com>
2021-12-30 15:15:16 +0100

    updating Dashboard app

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from Administration.models import Departement, Enseignant, Laboratoire, Sujet
+from Etudiant.models import Doctorant, Inscription
 def index(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
@@ -10,4 +12,14 @@ def index(request):
 
 @login_required(login_url=settings.LOGIN_URL)
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    data = {
+        'departements':Departement.objects.count(),
+        'enseignants':Enseignant.objects.count(),
+        'doctorants':Doctorant.objects.count(),
+        'inscriptions':Inscription.objects.count(),
+        'laboratoires':Laboratoire.objects.count(),
+        'sujets':Sujet.objects.count(),
+        'publications':0,
+        'formations':0,
+    }
+    return render(request, 'dashboard.html', data)
