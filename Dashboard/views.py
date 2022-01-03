b897e96c1208c92ddb6a2adf3507984e9f826c5e
A.mesbah <azdine.mesbah@gmail.com>
2022-01-03 15:33:09 +0100

    add statestics charts

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-
-from Administration.models import Departement, Enseignant, Laboratoire, Sujet
-from Etudiant.models import Doctorant, Inscription
+from Administration.models import Departement, Enseignant, Laboratoire, Sujet, Etablissement
+from CED_Tools.models import Pays, Annee
+from Etudiant.models import Doctorant, Inscription, Publication
 def index(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
@@ -13,13 +13,16 @@ def index(request):
 @login_required(login_url=settings.LOGIN_URL)
 def dashboard(request):
     data = {
-        'departements':Departement.objects.count(),
-        'enseignants':Enseignant.objects.count(),
-        'doctorants':Doctorant.objects.count(),
-        'inscriptions':Inscription.objects.count(),
-        'laboratoires':Laboratoire.objects.count(),
-        'sujets':Sujet.objects.count(),
-        'publications':0,
+        'departements':Departement.objects.all(),
+        'etablissements':Etablissement.objects.all(),
+        'enseignants':Enseignant.objects.all(),
+        'pays':Pays.objects.all(),
+        'doctorants':Doctorant.objects.all(),
+        'annees':Annee.objects.all()[:10],
+        'inscriptions':Inscription.objects.all(),
+        'laboratoires':Laboratoire.objects.all(),
+        'sujets':Sujet.objects.all(),
+        'publications':[sum([inscription.publications.count() for inscription in annee.inscriptions.all()]) for annee in Annee.objects.all()[:10]],
         'formations':0,
     }
     return render(request, 'dashboard.html', data)
